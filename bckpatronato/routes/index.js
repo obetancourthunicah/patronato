58954b680a03be82ae849ab3ae01ca677a32c0c0
Orlando J Betancourth <obetancourthunicah@gmail.com>
2020-06-18 18:57:23 -0600

    Acceso a Datos MongoDB Find, insertOne

--- bckpatronato/routes/index.js
+++ bckpatronato/routes/index.js
@@ -1,10 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var cuentasModel = require('./models/cuentas.model');
-cuentasModel.initModel();
-
-console.log(cuentasModel.getCuentas());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
