1059d675b0151bf4b383e7222a13dd5a044e2656
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-07-02 18:59:37 -0600

    Login y JWT

--- bckpatronato/routes/api/sec/index.js
+++ bckpatronato/routes/api/sec/index.js
@@ -1,6 +1,7 @@
 // Rutas de la Entidad de Seguridad
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 let secModel = require('./sec.model');
 
@@ -9,19 +10,39 @@ let  init = async ()=>{
 }
 init();
 
-router.get('/', function (req, res) {
-  res.status(200).json(
-    [
-      {
-        route: "/",
-        description: "Muestra Documentación del API de Seguridad",
-        body: "",
-        params: "",
-        resp: "json"
-      }
-    ]
-  )
-}); //get /
+router.post('/signin', async (req, res) => {
+  try {
+    var rslt = await secModel.addnew(req.body);
+    console.log(rslt);
+    res.status(200).json({ "msg": "Usuario Creado" });
+  } catch (err) {
+    res.status(500).json({ "error": "Algo Sucendió mal!!" });
+  }
+}
+);
+
+router.post('/login' , async(req, res)=>{
+  try {
+    var { email, pswd } = req.body;
+    //Hacer validaciones aqui
+    var user = await secModel.getByEmail(email);
+    if (await secModel.comparePassword(pswd, user.password)) {
+       const { email, roles, _id } = user;
+       const jUser = { email, roles, _id };
+       console.log(jUser);
+       let token = jwt.sign(jUser, process.env.JWT_SECRET, {expiresIn: '120m'});
+       res.status(200).json(
+         {
+           ...jUser, jwt: token
+         }
+       );
+    } else {
+      res.status(401).json({"error":"Credenciales Incorrectas"});
+    }
+  } catch (err) {
+    res.status(500).json({ "error": "Algo Sucendió mal!!" });
+  }
+});
 
 
 module.exports = router;
--- bckpatronato/routes/api/sec/sec.model.js
+++ bckpatronato/routes/api/sec/sec.model.js
@@ -1,5 +1,6 @@
 const db = require('../../dao/db');
 const ObjectId = require('mongodb').ObjectId;
+const bcrypt = require('bcrypt');
 
 let userColl;
 
@@ -20,4 +21,42 @@ module.exports = class {
     }
   } //initModel
 
+  static async addnew( data ){
+    const {email, password } = data;
+    try {
+      let newUser = {
+              "email": email,
+              "password" : bcrypt.hashSync(password, 10),
+              "lastlogin" : null,
+              "lastpasswordchanged" : null,
+              "passwordexpires" : new Date().getTime() + (1000 * 60 * 60 * 24 * 90),
+              "oldpasswords": [],
+              "roles": [ "public" ]
+      }
+      let rslt = await userColl.insertOne(newUser);
+      return rslt;
+    } catch (err){
+      console.log(err);
+      return err;
+    }
+  } // addnew
+
+  static async getByEmail(email){
+    try {
+        let filter = {"email": email};
+        let user = await userColl.findOne(filter);
+        return user;
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+
+  static async comparePassword( rawPassword, cryptedPassword) {
+    try {
+      return bcrypt.compareSync(rawPassword, cryptedPassword);
+    }catch (err){
+      return false;
+    }
+  }
 }
