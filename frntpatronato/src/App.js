6eb1c32fd097d0c8f2b291e24b251c73486f7db7
Orlando J Betancourth <orlando.betancourth@gmail.com>
2020-07-15 18:58:34 -0600

    Agregando react-router-dom para la navegación del spa

--- frntpatronato/src/App.js
+++ frntpatronato/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
 import Home from './components/Content/Home';
 import SignIn from './components/Content/SignIn';
 import Votes from './components/Content/Votes';
+
 function App() {
   return (
-    <Votes></Votes>
+    <Router>
+      <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/votes" component={Votes} exact />
+          <Route path="/signin" component={SignIn} exact />
+      </Switch>
+    </Router>
+    
   );
 }
 
--- frntpatronato/src/components/Footer/index.js
+++ frntpatronato/src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './footer.css';
 
 export default ()=>{
   return (
     <footer>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>Motions</li>
-          <li>Members</li>
-          <li>Notifications</li>
+          <li><NavLink to="/" >Home</NavLink></li>
+          <li><NavLink to="/signin">SignIn</NavLink></li>
+          <li><NavLink to="/votes">Votes</NavLink></li>
         </ul>
       </nav>
     </footer>
